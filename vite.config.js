dbe4f16fef25523125014da370008582e31c1386
justingreco <justin.greco@gmail.com>
2023-03-27 11:01:32 -0400

    added gh pages action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from "@vitejs/plugin-react";
+const config = {
+  base: '/street-name-application/',
+  plugins: [react()], 
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
+  server: {
+    open: true
+  }    
+};
+
+export default defineConfig(config);
